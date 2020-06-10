3bf4d6bd7d645856382999d2754c5c5f0f7e700d
SheronW <sheronw1174@gmail.com>
2020-06-09 22:19:46 -0400

    upgrade yarn

--- SheronW/src/pages/index.js
+++ SheronW/src/pages/index.js
@@ -1,21 +1,16 @@
 import React from "react";
 import classnames from "classnames";
 import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
 
 function Home() {
-  const context = useDocusaurusContext();
-  const { siteConfig = {} } = context;
-  /*title={`${siteConfig.title}`}*/
   return (
     <Layout description="SheronW 的自留地">
       <header className={classnames("hero hero--primary", styles.heroBanner)}>
         <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <h1 className="hero__title">Sheron W</h1>
+          <p className="hero__subtitle">CS student & DayDreamer</p>
+          <p className="hero__subtitle">大学生，码民工，常写无用文字。</p>
         </div>
       </header>
     </Layout>
